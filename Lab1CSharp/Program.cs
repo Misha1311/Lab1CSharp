0b2b4301192b0fe6aa597e89937a84d84ab5be92
Misha1311 <47081026+Misha1311@users.noreply.github.com>
2020-03-04 20:43:56 +0200

    Добавьте файлы проекта.

---
+++ Lab1CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter 1 number : ");
+            double kub1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2 number : ");
+            double kub2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Cube of the sum of 2 numbers = ");
+            Console.WriteLine(Math.Pow(kub1 + kub2, 3));
+        }
+    }
+}
